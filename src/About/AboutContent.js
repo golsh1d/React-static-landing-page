0a20cb492926bab044667d25accf65abe0897f9a
golshid <golsheedd@gmail.com>
2024-11-20 13:44:24 +0330

    update about section

--- src/About/AboutContent.js
+++ src/About/AboutContent.js
@@ -4,7 +4,7 @@ function AboutContent({percent, icon, text, lineWidth, mainColor}) {
 
     return(
         <div className="About-content w-80-About-content-max-width-768 w-30-About-content-min-width-768
-        mb-About-content-max-width-768">
+        mb-About-content-max-width-768 mb-About-content-min-width-768">
             <div className="About-content-under-line">
                 <div className="About-content-line" style={{width : lineWidth , backgroundColor : mainColor}}>
                 <div className="About-content-circle" style={{backgroundColor : mainColor}}></div>
